1c38b17800992fcd949d4361e9349ef9ae16a16a
hectorfranc0u <halejandrofb@gmail.com>
2023-06-08 09:13:08 -0600

    Login

---
+++ app/src/main/java/com/example/crunchyshoes/LoginActivity.kt
@@ -0,0 +1,54 @@
+package com.example.crunchyshoes
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+        loginButton = findViewById(R.id.loginButton)
+
+        auth = FirebaseAuth.getInstance()
+
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                signInWithEmailAndPassword(email, password)
+            } else {
+                Toast.makeText(this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun signInWithEmailAndPassword(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // El inicio de sesión fue exitoso, puedes redirigir a la siguiente actividad.
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // El inicio de sesión falló, muestra un mensaje de error.
+                    Toast.makeText(this, "Inicio de sesión fallido. Verifica tus credenciales.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
